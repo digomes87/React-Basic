bbaea62c279e00d59dae3b4a2116b2866f1514f3
Diego G <diego.gomes87@gmail.com>
2020-04-25 10:25:56 -0300

    Working with state

--- src/components/footer.js
+++ src/components/footer.js
@@ -14,11 +14,22 @@ class Footer extends Component{
         alert('OI clicou no alert');
     }
 
+    state = {
+        name: 'Diego Go'
+    }
+
+    message = evt => {
+        
+        this.setState({name: evt.target.value});
+        console.log(this.state.name);
+    }
+
     render(){
         return(
             <div>
                 <h1 onClick={this.createAlert}>Aqui um footer</h1>
                 <p>{this.props.page}</p>
+                <input value={this.state.name} onChange={this.message} />
             </div>
         )
     }
